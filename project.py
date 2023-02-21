6c7cd2770e02cd847c422715d9f0da538001cda0
Mark Hanna <mark@Marks-MacBook-Air-3.local>
2023-02-21 10:35:26 -0500

    Project skeleton

---
+++ project.py
@@ -0,0 +1,35 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+# Set environment variables for your credentials
+# Read more at http://twil.io/secure
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  body="Welcome to fake Duolingo\nTo get started, text back '1', to cancel text '0'",
+  from_="+18887099768",
+  to="+17819213699"
+)
+
+print(message.sid)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
